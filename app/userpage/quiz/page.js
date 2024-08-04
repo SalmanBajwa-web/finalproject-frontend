385736a9af316f4d88ce193e23c791a07f99d583
Salman <salman512512512@gmail.com>
2024-08-04 14:01:19 +0500

    fix deployment error

---
+++ app/userpage/quiz/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import UserPageNav from "@/components/mycomp/UserPageNav";
+
+export default function Dashboard() {
+ 
+
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <UserPageNav />
+
+      <p>This is quiz main page</p>
+    </div>
+  );
+}
